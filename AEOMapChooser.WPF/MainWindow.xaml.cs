a418cf6cc4f3be1fede8ebbc1d9573074922a54f
Fusion86 <fusion86@cerbus.nl>
2018-02-20 22:01:54 +0000

    Merge and export correct round name to markdown

--- AEOMapChooser.WPF/MainWindow.xaml.cs
+++ AEOMapChooser.WPF/MainWindow.xaml.cs
@@ -1,10 +1,12 @@
-﻿using AEOMapChooser.Core.Extensions;
+﻿using AEOMapChooser.Core.Enums;
+using AEOMapChooser.Core.Extensions;
 using AEOMapChooser.Core.Helpers;
 using AEOMapChooser.Core.Models;
 using AEOMapChooser.WPF.Models;
 using AEOMapChooser.WPF.ViewModels;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -30,10 +32,64 @@ namespace AEOMapChooser.WPF
             lvGeneratedRoundsView.GroupDescriptions.Add(generatedRoundsGroupDescription);
         }
 
-        private void btnBreak_Click(object sender, RoutedEventArgs e)
+        #region Commands
+
+        private void cmdGenerateRounds_CanExecute(object sender, CanExecuteRoutedEventArgs e)
         {
-            System.Diagnostics.Debugger.Break();
+            e.CanExecute = vm.SelectableMaps.Where(x => x.IsSelected).Count() > 1;
         }
+
+        private void cmdGenerateRounds_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            var maps = vm.SelectableMaps.Where(x => x.IsSelected && x.Type != SelectableMapType.Tiebreaker).Select(x => x.Map);
+
+            var rounds = MapChooser.GetRounds(
+                maps,
+                vm.NumberOfRounds,
+                vm.NumberOfMatchesPerRound,
+                true
+                );
+
+            vm.GeneratedRounds.Clear();
+
+            for (int i = 0; i < rounds.Count; i++)
+            {
+                string name = "Round " + (i + 1);
+                vm.GeneratedRounds.Add(new RoundViewModel(name, rounds[i]));
+            }
+
+            var tiebreakers = vm.SelectableMaps.Where(x => x.IsSelected && x.Type == SelectableMapType.Tiebreaker).Select(x => x.Map);
+            if (tiebreakers.Count() > 0)
+                vm.GeneratedRounds.Add(new RoundViewModel("Tiebreaker", new Round() { Maps = new[] { tiebreakers.Random() } }));
+        }
+
+        private void cmdCopyRoundsAsMarkdown_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = vm.GeneratedRounds.Count > 0;
+        }
+
+        private void cmdCopyRoundsAsMarkdown_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int roundNr = 0; roundNr < vm.GeneratedRounds.Count; roundNr++)
+            {
+                RoundViewModel round = vm.GeneratedRounds[roundNr];
+                sb.AppendLine($"**{round.Name}**");
+
+                for (int mapNr = 0; mapNr < vm.GeneratedRounds[roundNr].Maps.Length; mapNr++)
+                {
+                    Map map = round.Maps[mapNr];
+                    sb.Append($"| *{map.Type}:* ");
+                    sb.AppendLine(map.Name);
+                }
+                sb.AppendLine();
+            }
+
+            Clipboard.SetText(sb.ToString());
+        }
+
+        #endregion
 
         #region Selectable Maps Group Header
 
@@ -65,36 +121,7 @@ namespace AEOMapChooser.WPF
             cmdGenerateRounds_Executed(sender, null);
         }
 
-        private void cmdGenerateRounds_CanExecute(object sender, CanExecuteRoutedEventArgs e)
-        {
-            e.CanExecute = vm.SelectableMaps.Where(x => x.IsSelected && x.Type != SelectableMapType.Tiebreaker).Count() > 1;
-        }
-
-        private void cmdGenerateRounds_Executed(object sender, ExecutedRoutedEventArgs e)
-        {
-            var maps = vm.SelectableMaps.Where(x => x.IsSelected && x.Type != SelectableMapType.Tiebreaker).Select(x => x.Map);
-
-            var rounds = MapChooser.GetRounds(
-                maps,
-                vm.NumberOfRounds,
-                vm.NumberOfMatchesPerRound,
-                true
-                );
-
-            vm.GeneratedRounds.Clear();
-
-            for (int i = 0; i < rounds.Count; i++)
-            {
-                string name = "Round " + (i + 1);
-                vm.GeneratedRounds.Add(new RoundViewModel(name, rounds[i]));
-            }
-
-            var tiebreakers = vm.SelectableMaps.Where(x => x.IsSelected && x.Type == SelectableMapType.Tiebreaker).Select(x => x.Map);
-            if (tiebreakers.Count() > 0)
-                vm.GeneratedRounds.Add(new RoundViewModel("Tiebreaker", new Round() { Maps = new[] { tiebreakers.Random() } }));
-        }
-
-        private void iudNumRounds_ValueChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        private void generatorInput_ValueChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
         {
             cmdGenerateRounds_Executed(sender, null);
         }
